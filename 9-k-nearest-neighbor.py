58b179207ef0938f1e3c60cf522943832f5c57ac
Ashish Goswami <116054951+Ashish6298@users.noreply.github.com>
2024-07-28 01:18:03 +0530

    Update 9-k-nearest-neighbor.py

--- 9-k-nearest-neighbor.py
+++ 9-k-nearest-neighbor.py
@@ -1,19 +1,19 @@
-#KNN
-
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-
-# Load the dataset and split into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(load_iris()["data"], load_iris()["target"], random_state=0)
-
-# Initialize and train the KNeighborsClassifier
+import numpy as np
+dataset = load_iris()
+X_train,X_test,y_train,y_test=train_test_split(dataset["data"],dataset["target"],random_state=0)
 kn = KNeighborsClassifier(n_neighbors=3)
-kn.fit(X_train, y_train)
+kn.fit(X_train,y_train)
+KNeighborsClassifier(n_neighbors=3)
+prediction=kn.predict(X_test)
+confusion_matrix(y_test,prediction)
 
-# Make predictions and calculate confusion matrix
-prediction = kn.predict(X_test)
-cm = confusion_matrix(y_test, prediction)
+# output
+# array([[13,  0,  0],
+#        [ 0, 15,  1],
+#        [ 0,  0,  9]], dtype=int64)
 
-print("Confusion Matrix:\n", cm)
+
